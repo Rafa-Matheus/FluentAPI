99c8ef8645330d8bd727235024a1129af21fcbd0
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2024-12-05 17:19:25 -0300

    Initial setup for DbContext dependency injection and controllers mapping

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using FluentAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+namespace FluentAPI;
 
-app.Run();
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        
+        builder.Services.AddDbContext<AppDataContext>(options
+            => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        var app = builder.Build();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
