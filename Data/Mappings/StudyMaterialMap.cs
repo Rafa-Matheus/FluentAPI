bf32f54a6daf16d5f3d510cf13d7d1cb75f4b131
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2024-12-05 17:39:01 -0300

    Add mapping for StudyMaterial entity

---
+++ Data/Mappings/StudyMaterialMap.cs
@@ -0,0 +1,36 @@
+using FluentAPI.Model;
+using FluentAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluentAPI.Data.Mappings;
+
+public class StudyMaterialMap : IEntityTypeConfiguration<StudyMaterial>
+{
+    public void Configure(EntityTypeBuilder<StudyMaterial> builder)
+    {
+        builder.ToTable("StudyMaterials");
+        
+        builder.HasKey(x => x.StudyMaterialId);
+        builder.Property(x => x.StudyMaterialId)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+        
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasMaxLength(100)
+            .HasColumnName("MaterialTitle")
+            .HasColumnType("VARCHAR(100)");
+        
+        builder.Property(x => x.Description)
+            .IsRequired()
+            .HasMaxLength(500)
+            .HasColumnName("MaterialDescription")
+            .HasColumnType("VARCHAR(500)");
+
+        builder.HasOne(sm => sm.Course)
+            .WithMany(c => c.StudyMaterials)
+            .HasForeignKey(sm => sm.CourseId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
