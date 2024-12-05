2e4430ef9036794ced65719bfb55807326ae639f
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2024-12-05 17:39:18 -0300

    Add mapping for Teacher entity

---
+++ Data/Mappings/TeacherMap.cs
@@ -0,0 +1,34 @@
+using FluentAPI.Model;
+using FluentAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluentAPI.Data.Mappings;
+
+public class TeacherMap : IEntityTypeConfiguration<Teacher>
+{
+    public void Configure(EntityTypeBuilder<Teacher> builder)
+    {
+        builder.ToTable("Teachers");
+
+        builder.HasKey(t => t.TeacherId);
+        builder.Property(t => t.TeacherId)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+
+        builder.Property(t => t.Name)
+            .IsRequired()
+            .HasMaxLength(100)
+            .HasColumnName("TeacherName")
+            .HasColumnType("VARCHAR(100)");
+
+        // One-to-Many: Teacher -> Courses
+        builder.HasMany(t => t.Courses)
+            .WithOne(c => c.Teacher)
+            .HasForeignKey(c => c.TeacherId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.HasIndex(t => t.Name)
+            .HasDatabaseName("IX_Teachers_Name");
+    }
+}
